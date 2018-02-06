f634f49ee90c8375cddd1ccdc46be2e55235dae4
os-hussein <osman_990@hotmail.com>
2018-02-06 22:37:42 +0000

    further tests have failed, orange count returns zero

--- src/test/java/com/shop/TestOffers.java
+++ src/test/java/com/shop/TestOffers.java
@@ -22,4 +22,32 @@ public class TestOffers {
 		List<Item> items = shop.itemStringToList(Arrays.asList(itemStrArr));
 		assertEquals(3, shop.getItemCount(items, Apple.class));
 	}
+	
+	@Test
+	public void fiveApples(){
+		String[] itemStrArr = {"Apple", "Orange", "Apple", "Orange", "Apple", "Apple", "Apple"};
+		List<Item> items = shop.itemStringToList(Arrays.asList(itemStrArr));
+		assertEquals(5, shop.getItemCount(items, Apple.class));
+	}
+	
+	@Test
+	public void twoOranges(){
+		String[] itemStrArr = {"Apple", "Orange", "Apple", "Orange", "Apple", "Apple", "Apple"};
+		List<Item> items = shop.itemStringToList(Arrays.asList(itemStrArr));
+		assertEquals(2, shop.getItemCount(items, Orange.class));
+	}
+	
+	@Test
+	public void noOranges(){
+		String[] itemStrArr = {"Apple", "Apple", "Apple", "Apple", "Apple"};
+		List<Item> items = shop.itemStringToList(Arrays.asList(itemStrArr));
+		assertEquals(0, shop.getItemCount(items, Orange.class));
+	}
+	
+	@Test
+	public void noApples(){
+		String[] itemStrArr = {"Orange", "Orange", "Orange", "Orange"};
+		List<Item> items = shop.itemStringToList(Arrays.asList(itemStrArr));
+		assertEquals(0, shop.getItemCount(items, Apple.class));
+	}
 }
